a8ba702e4d15b1fde21493b02a568aadd8831a28
DJ-LP <Leon.Dorsch@schule1.de>
2026-05-19 08:37:21 +0200

    PacmanRatio

--- Pacman.java
+++ Pacman.java
@@ -8,12 +8,13 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Pacman extends Actor
 {
-    /**
-     * Act - do whatever the Pacman wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    public Pacman()
+    {
+        getImage().scale(PacManWorld.breite / 24, PacManWorld.hoehe / 16);
+    }
     public void act()
     {
+        
        
     }
 }
