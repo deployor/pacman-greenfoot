7d36a6009dec8c9ca5cb6ec8143c3132766cb1ae
DJ-LP <Leon.Dorsch@schule1.de>
2026-05-19 08:39:42 +0200

    PacMan Mitte

--- PacManWorld.java
+++ PacManWorld.java
@@ -14,7 +14,7 @@ public class PacManWorld extends World
         PacManWorld.hoehe = getHeight();
         //Greenfoot.playSound("PacmannMusikFürGreenFoot.mp3");
         
-        addObject(new Pacman(), getWidth(), getHeight());
+        addObject(new Pacman(), getWidth()/2, getHeight()/2);
         addObject(new settings(), getWidth() - (getWidth()/25), getHeight() - (getHeight()/25));
     }
 }
